2ea181d423ae1594c6a824d6cb7e11ac1c135428
huqj <2477889605@qq.com>
2018-08-15 18:11:34 +0800

    test vs commit

--- CSharpTest/Program.cs
+++ CSharpTest/Program.cs
@@ -8,12 +8,12 @@ namespace CSharpTest
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //礼节性的hello world
-            Console.WriteLine("Hello C#!");
-            //等待防止窗口关闭
-            Console.ReadLine();
-        }
+        //static void Main(string[] args)
+        //{
+        //    //礼节性的hello world
+        //    Console.WriteLine("Hello C#!");
+        //    //等待防止窗口关闭
+        //    Console.ReadLine();
+        //}
     }
 }
---
+++ CSharpTest/TestClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 测试c#的类，包括成员变量，成员方法和实例初始化等
+ */
+namespace CSharpTest
+{
+    class TestClass
+    {
+        //static void Main(String[] args)
+        //{
+        //    Dog dog = new Dog(12, "Lucky");
+        //    dog.Bark();
+        //    Console.Read();
+        //}
+    }
+
+    /*
+     * 创建一个类
+     */
+    class Dog
+    {
+        int age;
+        string name;
+
+        //构造方法
+        public Dog(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+            Console.WriteLine("create a dog with age=" + age + ",name=" + name);
+        }
+
+        //成员方法
+        public void Bark()
+        {
+            System.Console.WriteLine("wang wang wang~");
+        }
+
+    }
+}
