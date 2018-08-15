dfbec4c78e7c53bda427eda0a46807745eeff8f4
huqj <2477889605@qq.com>
2018-08-15 17:15:40 +0800

    init

---
+++ CSharpTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //礼节性的hello world
+            Console.WriteLine("Hello C#!");
+            //等待防止窗口关闭
+            Console.ReadLine();
+        }
+    }
+}
