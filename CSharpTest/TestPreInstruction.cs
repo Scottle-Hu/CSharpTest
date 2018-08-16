811d7fa8845f2251c77149447946aaf8d5999a73
huqj <2477889605@qq.com>
2018-08-16 17:59:39 +0800

    test pre instruction

---
+++ CSharpTest/TestPreInstruction.cs
@@ -0,0 +1,24 @@
+﻿#define FLAG  //定义标志
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//测试预处理指令
+namespace CSharpTest
+{
+    class TestPreInstruction
+    {
+        //static void Main(string[] args)
+        //{
+        //    //使用预处理指令进行条件编译
+        //    #if (FLAG)
+        //        Console.WriteLine("has Flag");
+        //    #else
+        //        Console.WriteLine("does not have Flag");
+        //    #endif
+        //    Console.Read();
+        //}
+    }
+}
