4136f5d1153ff551ee9bf42b04feeb609e298d8e
huqj <2477889605@qq.com>
2018-08-17 18:16:23 +0800

    test exception

---
+++ CSharpTest/TestException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//测试c#的异常处理
+namespace CSharpTest
+{
+    class TestException
+    {
+    }
+}
